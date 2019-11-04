2dac4b1dc4554fa0688bca750c8ebdf96a7e516a
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-11-04 19:18:30 +0100

    Move `sys` import up

--- recipe/run_tests.py
+++ recipe/run_tests.py
@@ -1,3 +1,5 @@
+import sys
+
 import pyglet
 import pyglet.app
 import pyglet.canvas
@@ -22,7 +24,6 @@ import pyglet.text.formats
 # import pyglet.media.drivers.pulse
 
 # Platform-specific tests.
-import sys
 
 if sys.platform == 'win32':
     import pyglet.libs.win32
